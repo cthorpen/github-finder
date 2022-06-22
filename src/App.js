40c7507e8b432afc23901399b3527aad028a0202
Cole Thorpen <thorpencole@gmail.com>
2022-06-22 13:00:11 -0700

    converted to hooks

--- src/App.js
+++ src/App.js
@@ -1,105 +1,43 @@
-import React, { Component, Fragment } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
-import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
+
+import Home from './components/pages/Home';
 import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
+
 import User from './components/users/User';
-import axios from 'axios';
+
+import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
+
 import './App.css';
 
-class App extends Component {
-  state = {
-    users: [],
-    user: {},
-    loading: false,
-    alert: null
-  }
+const App = () => {
 
-  // async componentDidMount() {
-  //   this.setState({ loading: true});
-
-  //   const res = await axios.get(`https://api.github.com/users?client_id=
-  //     ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
-  //     ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-  //   this.setState({users: res.data, loading: false});
-  // }
-
-  // search the Github Users. make api call
-  searchUsers = async text => {
-    this.setState({ loading: true });
-    const res = await axios.get(`https://api.github.com/search/users?q=
-      ${text}&client_id=
-      ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
-      ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-    this.setState({users: res.data.items, loading: false});
-  }
-
-  // get single Github user
-  getUser = async username => {
-    this.setState({ loading: true });
-
-    const res = await axios.get(`https://api.github.com/users/${username}?&client_id=
-      ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
-      ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-    this.setState({user: res.data, loading: false});
-  } 
-
-  // clear users from state
-  clearUsers = () => this.setState({ users: [], loading: false})
-
-  // setting alert for empty search
-  setAlert = (msg, type) => {
-    this.setState({ alert: {msg, type } })
-    setTimeout(() => this.setState({ alert: null }), 5000)
-  }
-
-  render() {
-    const { users, loading, user } = this.state;
     return (
-      <Router>
-        <div className='App'>
-          <Navbar title="Github Finder!" icon="fab fa-github" />
+      <GithubState>
+        <AlertState>
+            <Router>
+            <div className='App'>
+              <Navbar title="Github Finder!" icon="fab fa-github" />
 
-          <div className="container">
-            <Alert alert={this.state.alert} />
-            <Routes>
-
-              <Route exact path="/" element={ ( // props => () ... might need
-                <Fragment>
-                  <Search 
-                    searchUsers={this.searchUsers} 
-                    clearUsers={this.clearUsers} 
-                    showClear={users.length > 0 ? true : false} 
-                    setAlert={this.setAlert}
-                  />
-                  <Users 
-                    loading={loading} 
-                    users={users} 
-                  />
-                </Fragment>
-              )} />
-
-              <Route exact path='/about' element={<About/>} />
-
-              <Route exact path='/user/:login' render={props => (
-                <User 
-                  { ...props }
-                  getUser={this.getUser} 
-                  user={user} 
-                  loading={loading} />
-              )} />
-
-            </Routes>
-          </div>
-        </div>
-      </Router>
+              <div className="container">
+                <Alert />
+                <Switch>
+                  <Route exact path='/' element={<Home/>} />
+                  <Route exact path='/about' element={<About/>} />
+                  <Route exact path='/user/:login' element={<User/>} />
+                  <Route element={<NotFound/>} />
+                </Switch>
+              </div>
+            </div>
+          </Router>
+        </AlertState>
+      </GithubState>
+      
     );
-  }
 
 }
 
